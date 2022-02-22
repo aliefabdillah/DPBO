db1c937277a716e54a3d135e4ba822ae3ff03298
aliefabdillah <aliefmabdillah09@gmail.com>
2022-02-22 14:25:02 +0700

    modif contoh soal 1 php (succed)

--- php/4th_week/contohsoal2/IndexMatriks.php
+++ php/4th_week/contohsoal2/IndexMatriks.php
@@ -1,77 +1,78 @@
 <?php
-include "Matriks.php";
+
+
+include("Matriks.php");
 
 /* inisialisasi data yang diterima jik ada kiriman sebelumnya */
-$baris = $_REQUEST["Baris"] ?? "";
-$kolom = $_REQUEST["Kolom"] ?? "";
+$baris = $_REQUEST["baris"] ?? "";
+$kolom = $_REQUEST["kolom"] ?? "";
 $mat = $_REQUEST["mat"] ?? "";
 
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Matriks</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matriks</title>
 </head>
-
 <body>
-<!-- form masukan untuk user -->
-<form method="post">
-    <table>
-        <tr>
-            <td>Baris</td>
-            <td>:</td>
-            <td><input type="text" name="baris" value="<?php echo $baris;?>"/></td>
-        </tr>
-        <tr>
-            <td>Kolom</td>
-            <td>:</td>
-            <td><input type="text" name="kolom" value="<?php echo $kolom;?>"/></td>
-        </tr>
-   
-<?php
-    //jika telah ada masukan
-    if ((!empty($baris)) && (!empty($kolom))) {
-        $m = new Matriks();
-        $m->setBaris($baris);
-        $m->setKolom($kolom);
-        
-        //jika matriks telah di isi
-        if (isset($mat[0][0])) {
-            $mat->setMat($mat);
-        }
+    <!-- form masukan untuk user -->
+    <form method="post">
+        <table>
+            <tr>
+                <td>Baris</td>
+                <td>:</td>
+                <td><input type="text" name="baris" value="<?php echo $baris;?>"></td>
+            </tr>
+            <tr>
+                <td>Kolom</td>
+                <td>:</td>
+                <td><input type="text" name="kolom" value="<?php echo $kolom;?>"></td>
+            </tr>
+        <?php
+            //jika telah ada masukan
+            if ((!empty($baris)) && (!empty($kolom))) {
+                $m = new Matriks();
+                $m->setBaris($baris);
+                $m->setKolom($kolom);
+            
+                //jika matriks telah di isi
+                if (isset($mat[0][0])) {
+                    $m->setMat($mat);
+                }
 
-        //menampilkan isi matriks
-        for($i=0; $i<$baris; $i++){
-            for($j=0; $j<$kolom; $j++){
-                ?>
-
-                <tr>
-                    <td>Matriks [<?php echo $i; ?>][<?php echo $j; ?>]</td>
-                    <td>:</td>
-                    <td><input type="text" name="mat[<?php echo $i; ?>][<?php echo $j; ?>]" value="<?php if(isset($m->getMat()[0][0])){ echo $m->getMat()[$i][$j];} ?>"/></td>
-                </tr>
-                <?php
+                //menampilkan isi matriks
+                for($i=0; $i<$baris; $i++){
+                    for($j=0; $j<$kolom; $j++){
+                        ?>
+                        <tr>
+                            <td>Matriks [<?php echo $i; ?>][<?php echo $j; ?>]</td>
+                            <td>:</td>
+                            <td><input type="text" name="mat[<?php echo $i; ?>][<?php echo $j; ?>]" value="<?php if(isset($m->getMat()[0][0])){ echo $m->getMat()[$i][$j];} ?>"></td>
+                        </tr>
+                        <?php
+                    }
+                }
             }
+        ?>
+            <tr>
+                <td span="3"><input type="submit" name="buat" value="  Kirim  "></td>
+            </tr>
+        </table>
+    </form>
+    <?php
+    // menampilkan isi matriks jika telah terisi
+    if (isset($mat[0][0])) {
+        for ($i=0; $i < $baris; $i++) { 
+            for ($j=0; $j < $kolom; $j++) { 
+                echo $m->getMat()[$i][$j]." ";
+            }
+            echo "<br/>";
         }
-    }
-?>
-
-        <tr>
-            <td span="3"><input type="submit" name="buat" value="  Kirim  "/></td>
-        </tr>
-    </table>
-</form>
-<?php
-// menampilkan isi matriks jika telah terisi
-if (isset($mat[0][0])) {
-    for ($i=0; $i < $baris; $i++) { 
-        for ($j=0; $j < $kolom; $j++) { 
-            echo $m->getMat()[$i][$j]." ";
-        }
-        echo "<br/>";
-    }
-}
-
-?>
+    }   
+    ?>
 </body>
 </html>
--- php/4th_week/contohsoal2/Matriks.php
+++ php/4th_week/contohsoal2/Matriks.php
@@ -1,4 +1,5 @@
 <?php
+
 class Matriks {
     private $baris;
     private $kolom;
